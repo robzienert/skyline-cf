9354c4a36e369f2529113a1ed591f68f12ee58ed
Rob Zienert <rzienert@bloomhealthco.com>
2014-05-23 12:54:11 -0500

    Adding berks deps to metadata

--- chef/metadata.rb
+++ chef/metadata.rb
@@ -6,5 +6,9 @@ description       'Playing Skyline to learn more about chef and friends'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.1.0'
 
+depends 'git'
+depends 'python'
+depends 'redis'
+depends 'scipy'
 depends 'skyline'
 supports 'ubuntu'
